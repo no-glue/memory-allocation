cae2a6741a7d154a42453d32c3d8d7b0a30f050d
no-glue <nikola_pav@eunet.rs>
2014-11-03 00:09:10 +0100

    next fit

--- memory_list_next_fit.h
+++ memory_list_next_fit.h
@@ -1,4 +1,33 @@
 #include "memory_list.h"
 template<class Node, typename Type>class MemoryListNextFit:public <Node, Type>MemoryList {
-
+public:
+  MemoryListNextFit:MemoryList() {}
+  MemoryListNextFit(unsigned int max_size, unsigned int size):MemoryList(max_size, size), position(NULL) {}
+  void insert(char * key) {insert(key, size, max_size, position, head, tail);}
+private:
+  Node * position;
+  void insert(char * key, unsigned int & size, unsigned int max_size, Node * & position, Node * & head, Node * & tail) {
+    // insert key to list
+    Node * new_one = new Node(key);
+    Node * blank;
+    Node * temp = head;
+    while(temp) {
+      if(temp->is_blank() && new_one->size > temp->max_size) {
+        temp = temp->right;
+        continue;
+      }
+      if(temp->is_blank() && new_one->size <= temp->max_size) {
+        blank = new Node("", temp->max_size - new_one->size);
+        replace_node(temp, new_one, size, max_size, head, tail);
+        insert_after_node(new_one, blank, size, max_size, head, tail);
+        position = new_one;
+        delete temp;
+        return;
+      }
+    }
+    blank = new Node("", new_one->remaining());
+    insert_right_node(new_one, size, max_size, head, tail);
+    insert_right_node(blank, size, max_size, head, tail);
+    position = new_one;
+  }
 };
